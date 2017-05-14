f03e0e37c01d795244e80a58ffb978f9516bc898
Andrew Gardner <andrewgardner@Andrews-MacBook-Air-2.local>
2017-05-14 02:57:55 -0700

    heroku not working

--- config/connection.js
+++ config/connection.js
@@ -2,37 +2,27 @@
 
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-// 	host: "localhost",
-// 	user: "root",
-// 	password: "",
-// 	database: "burgers_db"
-// });
-
-
-var pool = mysql.createPool({
-    host: 'us-cdbr-iron-east-03.cleardb.net',
-    port: 8080,
-    user: 'b87ebaba5a0441',
-    password: 'accord',
-    database: 'heroku_7e15a788481c000',
-    connectionLimit: 5
-  });
-
-
-pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+var connection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "accord",
+	database: "burgers_db"
 });
 
-module.exports= pool;
+// ClearDB object
+// var connection = mysql.createConnection({
+//     host: 'us-cdbr-iron-east-03.cleardb.net',
+//     user: 'b87ebaba5a0441',
+//     password: 'accord',
+//     database: 'heroku_7e15a788481c000'
+//   });
 
-// connection.connect(function(err) {
-// 	if (err) {
-// 		console.log("error connecting: " + err.stack);
-// 		return;
-// 	}
-// 	console.log("connected as id: " + connection.threadId);
-// });
+connection.connect(function(err) {
+	if (err) {
+		console.log("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id: " + connection.threadId);
+});
 
-// module.exports = connection;
+module.exports= connection;

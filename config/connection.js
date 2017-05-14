00c2907d9d250f061c3ede8b1f9f4758e79b15ea
Andrew Gardner <andrewgardner@Andrews-MacBook-Air-2.local>
2017-05-14 02:32:04 -0700

    trying to deploy to heroku with cleardb

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,38 @@
-// This code connects Node to MySQL
+// // This code connects Node to MySQL
 
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
+// var connection = mysql.createConnection({
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "",
+// 	database: "burgers_db"
+// });
+
+
+var pool = mysql.createPool({
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    port: 8080,
+    user: 'b87ebaba5a0441',
+    password: '',
+    database: 'heroku_7e15a788481c000',
+    connectionLimit: 5
+  });
+
+
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
 });
 
-connection.connect(function(err) {
-	if (err) {
-		console.log("error connecting: " + err.stack);
-		return;
-	}
-	console.log("connected as id: " + connection.threadId);
-});
+module.exports= pool;
 
-module.exports = connection;
+// connection.connect(function(err) {
+// 	if (err) {
+// 		console.log("error connecting: " + err.stack);
+// 		return;
+// 	}
+// 	console.log("connected as id: " + connection.threadId);
+// });
+
+// module.exports = connection;
